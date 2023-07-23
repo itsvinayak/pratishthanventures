7218694677f486063c9237377d0027e08cdeac6b
vinayak <itssvinayak@gmail.com>
2023-07-23 21:55:15 +0530

    lint fixes

--- components/experience/counter.tsx
+++ components/experience/counter.tsx
@@ -1,12 +1,12 @@
 import { useRef } from "react";
 import { useCountUp } from "react-countup";
 
-const easingFn = function (t, b, c, d) {
+const easingFn = function (t: number, b: number, c: number, d: number) {
     // easeInOutExpo easing
     return c * (Math.pow(t / d - 1, 5) + 1) + b;
 };
 
-const Counter = (props) => {
+const Counter = (props: { styles?: null | undefined; data: { startNum?: 0 | undefined; endNum?: 10 | undefined; duration?: 5 | undefined; delay?: 1 | undefined; }; }) => {
     const {
         styles = null,
         data: { startNum = 0, endNum = 10, duration = 5, delay = 1 }
